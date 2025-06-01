f2905bb1567963448393c37ee22b37fa097e983f
Akaikumogo <98486553+Akaikumogo@users.noreply.github.com>
2025-06-01 06:11:27 +0500

    done

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -42,6 +42,7 @@ export class AuthService {
     const payload = { username: user.username, sub: user._id };
     return {
       access_token: this.jwtService.sign(payload),
+      user: user,
     };
   }
 
